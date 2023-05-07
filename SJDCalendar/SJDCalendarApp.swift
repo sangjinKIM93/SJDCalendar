b11da0223558e635d0bb1f8479e83fcdf30f7b7b
김상진, Kim Sang Jin <62230118+sangjinKIM93@users.noreply.github.com>
2023-05-07 10:37:46 +0900

    Initial Commit

---
+++ SJDCalendar/SJDCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SJDCalendarApp.swift
+//  SJDCalendar
+//
+//  Created by 김상진 on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct SJDCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
