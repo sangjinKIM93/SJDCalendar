3fd5e358315cbf487cb058ca1ce49c337833f12b
김상진, Kim Sang Jin <62230118+sangjinKIM93@users.noreply.github.com>
2023-05-12 20:06:53 +0900

    mock 데이터 수정

--- SJDCalendar/ViewModel/CalendarViewModel.swift
+++ SJDCalendar/ViewModel/CalendarViewModel.swift
@@ -10,12 +10,12 @@ import Foundation
 class CalendarViewModel: ObservableObject {
     
     @Published var goalList = [
-        DailyGoal(date: "5/1", list: ["스위프트유아이 공부", "코테 공부","스위프트유아이 공부", "코테 공부","스위프트유아이 공부", "코테 공부"]),
-        DailyGoal(date: "5/2", list: ["스위프트유아이 공부", "코테 공부"]),
-        DailyGoal(date: "5/3", list: ["스위프트유아이 공부", "코테 공부"]),
-        DailyGoal(date: "5/4", list: ["스위프트유아이 공부", "코테 공부"]),
-        DailyGoal(date: "5/5", list: ["스위프트유아이 공부", "코테 공부"]),
-        DailyGoal(date: "5/6", list: ["스위프트유아이 공부", "코테 공부"]),
-        DailyGoal(date: "5/7", list: ["스위프트유아이 공부", "코테 공부"]),
+        DailyGoal(date: "5/1", list: [""]),
+        DailyGoal(date: "5/2", list: [""]),
+        DailyGoal(date: "5/3", list: [""]),
+        DailyGoal(date: "5/4", list: [""]),
+        DailyGoal(date: "5/5", list: [""]),
+        DailyGoal(date: "5/6", list: [""]),
+        DailyGoal(date: "5/7", list: [""]),
     ]
 }
